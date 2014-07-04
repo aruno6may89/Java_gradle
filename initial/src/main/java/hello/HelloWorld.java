dcf06718b0e777202c6c5f8ec8a0fa311a9bdbae
aruno6may <arunkumar06may89@gmail.com>
2014-07-04 11:35:06 +0530

    duplicate class

--- initial/src/main/java/hello/HelloWorld.java
+++ initial/src/main/java/hello/HelloWorld.java
@@ -6,3 +6,9 @@ public class HelloWorld {
     System.out.println(greeter.sayHello());
   }
 }
+public class HelloWorld {
+  public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+}
